dd5ac59f30edcfe8f6daf32d4dafc69453e31719
kigabit <kigabit@gmail.com>
2017-04-08 11:53:50 -0700

    Remove extra comments

--- CinemagraphMode.lua
+++ CinemagraphMode.lua
@@ -1,4 +1,3 @@
-repeat_period = 60 -- how long each video show run (across multiple repeats) in seconds
 play_count_limit = 4
 
 function descriptor()
@@ -24,7 +23,6 @@ end
 function meta_changed()
     -- related to capabilities={"meta-listener"} in descriptor()
     -- triggered by available media input meta data?
-    --update_playback_mode()
     play_count = play_count + 1
     update_playback_mode()
 end
@@ -40,7 +38,7 @@ function update_playback_mode()
             local meta = item:metas()
 
             if meta then
-                vlc.msg.dbg("Playcount "..play_count.." for "..item:name())
+                vlc.msg.dbg("Playcount "..play_count.." for "..item:name().." (time played: ".. ..")")
 
                 if play_count == 12 and previous_item ~= item:name() then vlc.playlist.repeat_("on") end
 
@@ -61,31 +59,3 @@ end
 function close()
     vlc.deactivate()
 end
-
---[[function update_playback_mode()
-  if vlc.input.is_playing() then
-    local item = vlc.item or vlc.input.item()
-    if item then
-      local meta = item:metas()
-      if meta then
-        local repeat_track = meta["Playback mode"]
-        if repeat_track == nil then
-          repeat_track = false
-        elseif string.lower(repeat_track) == "repeat" then
-          repeat_track = true
-        else
-          repeat_track = false
-        end
-
-    local player_mode = vlc.playlist.repeat_()
-
-    -- toggle playlist.repeat_() as required        
-    if player_mode and not repeat_track then
-        vlc.playlist.repeat_()
-    elseif not player_mode and repeat_track then
-        vlc.playlist.repeat_()
-    end
-
-    return true
-  end
-end]]--
